d53d07ec585b7a118298e43869f79da1f1fb22b6
ahmawad <ahmawad.aa@gmail.com>
2024-04-11 13:22:47 -0500

    utilized functions for testing

--- BasicCalculator/src/main/java/org/example/Main.java
+++ BasicCalculator/src/main/java/org/example/Main.java
@@ -7,10 +7,10 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Enter the first number:");
-        float num1 = scanner.nextFloat();
+        int num1 = scanner.nextInt();
 
         System.out.println("Enter the second number:");
-        float num2 = scanner.nextFloat();
+        int num2 = scanner.nextInt();
 
         scanner.nextLine();
 
@@ -26,17 +26,39 @@ public class Main {
 
 
         if(choice.equalsIgnoreCase("a")){
-            System.out.println(num1 + "+" + num2 + "=" + (num1+num2));
+            int sum = addition(num1, num2);
+            System.out.println(num1 + "+" + num2 + "=" + sum);
         }
         else if (choice.equalsIgnoreCase("s")) {
-            System.out.println(num1 + "-" + num2 + "=" + (num1-num2));
+            int diff = subtraction(num1, num2);
+            System.out.println(num1 + "-" + num2 + "=" + diff);
         }
         else if (choice.equalsIgnoreCase("m")) {
-            System.out.println(num1 + "*" + num2 + "=" + (num1*num2));
+            int prod = product(num1, num2);
+            System.out.println(num1 + "*" + num2 + "=" + prod);
         }
         else if (choice.equalsIgnoreCase("d")) {
-            System.out.println(num1 + "/" + num2 + "=" + (num1/num2));
+            int quot = quotient(num1, num2);
+            System.out.println(num1 + "/" + num2 + "=" + quot);
         }
 
     }
+    public static int addition (int num1, int num2){
+        int sum = num1 + num2;
+        return sum;
+    }
+    public static int subtraction (int num1, int num2){
+        int diff = num1 - num2;
+        return diff;
+    }
+    public static int product (int num1, int num2){
+        int prod = num1 * num2;
+        return prod;
+    }
+    public static int quotient (int num1, int num2){
+        int quot = num1 / num2;
+        return quot;
+    }
+
+
 }
